a3cc21f6d104a3f3c54cd254f0598cbc34617a16
defaultujjjk <160222484+UJ1K@users.noreply.github.com>
2024-06-21 22:56:42 +0700

    Update index.jsx

--- src/components/MenuUser/index.jsx
+++ src/components/MenuUser/index.jsx
@@ -57,24 +57,24 @@ const MenuUser = () => {
           color: "#999999",
         }}
       >
-        <p>Fauzi Salam</p>
-        <p>22/503617/GE/09989</p>
-        <p>Ridlo Reiga Ahadi</p>
-        <p>22/497475/GE/09893</p>
-        <p>Rinasari Wijayanti</p>
-        <p>22/493874/GE/09834</p>
-        <p>Kuntari Kadek Nailah</p>
-        <p>22/493133/GE/09797</p>
-        <p>Sultan Derry Mahapani S.</p>
-        <p>22/496440/GE/09861</p>
         <p>Ananda Shabrina Putri G.</p>
-        <p>22/505172/GE/10055</p>
-        <p>Muhammad Ghifari Fadhillah</p>
-        <p>22/504497/GE/10027</p>
-        <p>Salvia Umi Chamidah</p>
-        <p>22/502633/GE/09954</p>
-        <p>Hajar Alya Arifah</p>
-        <p>22/498412/GE/09910</p>
+    <p>&bull; 22/505172/GE/10055</p>
+    <p>Fauzi Salam</p>
+    <p>&bull; 22/503617/GE/09989</p>
+    <p>Hajar Alya Arifah</p>
+    <p>&bull; 22/498412/GE/09910</p>
+    <p>Kuntari Kadek Nailah</p>
+    <p>&bull; 22/493133/GE/09797</p>
+    <p>Muhammad Ghifari Fadhillah</p>
+    <p>&bull; 22/504497/GE/10027</p>
+    <p>Ridlo Reiga Ahadi</p>
+    <p>&bull; 22/497475/GE/09893</p>
+    <p>Rinasari Wijayanti</p>
+    <p>&bull; 22/493874/GE/09834</p>
+    <p>Salvia Umi Chamidah</p>
+    <p>&bull; 22/502633/GE/09954</p>
+    <p>Sultan Derry Mahapani S.</p>
+    <p>&bull; 22/496440/GE/09861</p>
       </span>
     </div>
   )
