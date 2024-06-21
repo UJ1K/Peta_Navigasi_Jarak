6cf6da6ee81a356f411a0b52d8b1b09ad4d619da
defaultujjjk <160222484+UJ1K@users.noreply.github.com>
2024-06-21 22:35:23 +0700

    Update index.jsx

--- src/components/MenuUser/index.jsx
+++ src/components/MenuUser/index.jsx
@@ -53,7 +53,7 @@ const MenuUser = () => {
       </span>
       <span
         css={{
-          fontSize: "0.85rem",
+          fontSize: "0.55rem",
           color: "#999999",
         }}
       >
